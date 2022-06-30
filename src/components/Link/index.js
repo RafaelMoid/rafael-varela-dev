776a384e0d88264085f1c162b07618286528065a
Rafael Varela <rafaelvrcr@gmail.com>
2022-06-30 14:21:36 -0300

    <FIX> [Props added to NextLink]

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -2,6 +2,6 @@ import NextLink from 'next/link'
 
 export default function Link(children, href, ...props) {
     <NextLink href={href} passHref>
-        <a>{children}</a>
+        <a {...props}>{children}</a>
     </NextLink>
 }
