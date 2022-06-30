91e66f598894a1729ee26e3a2cf8761f9201e827
Rafael Varela <rafaelvrcr@gmail.com>
2022-06-30 17:38:12 -0300

    portfolio refactor

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,12 +1,18 @@
-import Link from '../src/components/Link';
+ import HomeScreen from "../src/screens/HomeScreen";
 
-export default function HomePage() {
-    return (
-        <div>
-            <h1>Rafael Varela - FAQ</h1>
-            <Link href="/" passHref>
-                Ir para a HOME
-            </Link>
-        </div>
-    )
-}
+export default HomeScreen;
+
+
+
+// import Link from '../src/components/Link';
+
+// export default function HomePage() {
+//     return (
+//         <div>
+//             <h1>Rafael Varela - FAQ</h1>
+//             <Link href="/" passHref>
+//                 Ir para a HOME
+//             </Link>
+//         </div>
+//     )
+// }
