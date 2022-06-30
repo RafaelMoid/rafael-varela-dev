d51f340d8d9f6c59e109d932f5e63739326f0524
Rafael Varela <rafaelvrcr@gmail.com>
2022-06-30 16:51:19 -0300

    Lib added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f49dc3ae614abdcc"; }
+/******/ 		__webpack_require__.h = function() { return "ef26e7b6a4230676"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
