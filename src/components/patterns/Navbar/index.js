1975c5af90e3888ba3e21ec658011d81cdd75109
Rafael Varela <rafaelvrcr@gmail.com>
2022-07-15 11:46:42 -0300

    <FIX> [error]

--- src/components/patterns/Navbar/index.js
+++ src/components/patterns/Navbar/index.js
@@ -1,7 +1,20 @@
-import { theme } from '../../../theme/theme';
-import { Box, Text, Image } from '../../../theme/components';
-import Link from '../../Link'
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import { corPrimaria } from "../UI/variaveis";
+
+const StyledCabecalho = styled.nav`
+  background-color: ${corPrimaria};
+  display: flex;
+  justify-content: space-between;
+  padding: 0 15vw;
+  height: 10vh;
+  align-items: center;
+`;
+
+const Logo = styled.img`
+  height: 50px;
+  width: 50px;
+`;
 
 const StyledUl = styled.ul`
     list-style-type: none;
@@ -15,72 +28,20 @@ const StyledLi = styled.li`
 `;
 
 
-export default function Navbar() {
+const Navbar = () => {
   return (
-    <Box
-      as="footer"
-      styleSheet={{
-        backgroundColor: theme.colors.primary[100]
-      }}
-    >
-      <Box
-        styleSheet={{
-          overflow:"hidden",
-          maxWidth: theme.space.xcontainer_xl,
-          marginLeft: "auto",
-          marginRight:"auto",
-          paddingVertical:{
-            xs: theme.space.x12,
-          },
-          paddingHorizontal:{
-            xs: theme.space.x4,
-            sm: theme.space.x6,
-            lg: theme.space.x8,
-          },
-        }}
-      >
-            {/* Dando o seguinte erro: Server Error
-                        ReferenceError: Image is not defined
+    <StyledCabecalho>
+      <Logo src="/images/logoComNomeRoxo.png" alt="Logo Smart Bank" />
+      <div>
+        <StyledUl>
+                  <StyledLi><Link href="/" ><Image src="/images/logoComNomeRoxo.png"/></Link></StyledLi>
+                  <StyledLi><Link href="/" >Home</Link></StyledLi>
+                  <StyledLi><Link href="/portfolio" >Portfolio</Link></StyledLi>
+                  <StyledLi><Link href="/contato" >Contato</Link></StyledLi>
+              </StyledUl>
+        </div>
+    </StyledCabecalho>
+  );
+};
 
-                        This error happened while generating the page. Any console logs will be displayed in the terminal window.
-                        Source
-                        src\components\patterns\Navbar\index.js (60:22) @ Navbar
-
-                          58 |       }}
-                          59 |       src={LOGO_RAFAEL_VARELA_URL_NAV}
-                        > 60 |       alt="Logo Rafael Varela da Nav"
-                             |          ^
-                          61 |     />
-                          62 |     <h1>Rafael Varela</h1>
-                          63 | </Box> */}
-            {/* <Box styleSheet={{
-                      display: "inline-flex",
-                      alignItems: {
-                        xs: "flex-start",
-                        sm: "center",
-                      },
-                      flexDirection: {
-                        xs: "column",
-                        sm: "row",
-                      },
-                    }}>
-                <Image
-                  styleSheet={{
-                    width: "auto",
-                    height: theme.space.x10,
-                  }}
-                  src={LOGO_RAFAEL_VARELA_URL_NAV}
-                  alt="Logo Rafael Varela da Nav"
-                />
-                <h1>Rafael Varela</h1>
-            </Box> */}
-            <StyledUl>
-                <StyledLi><Link href="/" ><Image src="/images/logoComNomeRoxo.png"/></Link></StyledLi>
-                <StyledLi><Link href="/" >Home</Link></StyledLi>
-                <StyledLi><Link href="/portfolio" >Portfolio</Link></StyledLi>
-                <StyledLi><Link href="/contato" >Contato</Link></StyledLi>
-            </StyledUl>
-      </Box>
-    </Box>
-  )
-}
+export default Navbar;
